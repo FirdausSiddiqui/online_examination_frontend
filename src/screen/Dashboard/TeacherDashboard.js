5eeaeebb5ae49ab2aa7f11c7f347f635698bfb48
Sreejit De <sreejitde7@gmail.com>
2021-06-02 00:58:23 +0530

    Added card for New Exam Paper

--- src/screen/Dashboard/TeacherDashboard.js
+++ src/screen/Dashboard/TeacherDashboard.js
@@ -1,10 +1,23 @@
 import { Container, Row } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import ExamCard from '../../components/ExamCard';
 import styles from './dashboard.module.css';
 
 const TeacherDashboard = () => {
   return (
-    <main>
+    <main className={styles.dashboard}>
+      <section className="mb-4">
+        <h2>Welcome,</h2>
+        <h1>Sreejit De.</h1>
+      </section>
+      <Container>
+        <Row>
+          <NavLink className="link" to="/auth/uploadQuestion">
+            <ExamCard code="New Exam Paper" forNewExam={true} />
+          </NavLink>
+        </Row>
+      </Container>
+      <h4 className="mt-4">Previous Papers</h4>
       <Container className={styles.container}>
         <Row xs={1} sm={2} md={3} lg={4} xl={5}>
           <ExamCard code="CS 802D" title="1st Internal" />
